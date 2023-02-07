76ab9cc31414c2f5a968c09a902b09487979a4de
rfhusv <rfhusv@gmail.com>
2023-02-07 12:30:31 +0400

    Simple CSS changes | Create JS functions that add rows and cells (create NxN canvas table)

--- javascript.js
+++ javascript.js
@@ -1,2 +1,21 @@
+function addRows (size) {
+    for (let i = 0; i < size; i++) {
+        const canvas = document.querySelector('.canvas');
+        const row = document.createElement('div');
+        row.classList.add(`row${i}`);
+        canvas.appendChild(row);
+    }
+}
 
+function addCells (size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.querySelector(`.row${i}`);
+        row.setAttribute('style', 'display : flex; flex : 1;')
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cells');
+            row.appendChild(cell);
+        }
+    }
+}
 
